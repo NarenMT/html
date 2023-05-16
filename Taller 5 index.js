b1a60bb7bc1f312c2483f442c3f67eecc0d38359
NarenMT <55023485+NarenMT@users.noreply.github.com>
2023-05-15 21:51:58 -0500

    Create Taller 5 index.js

---
+++ Taller 5 index.js
@@ -0,0 +1,112 @@
+// CONDICIONALES es para toma de decisiones para nuestro software 
+
+//IF (condición){
+//      console.log ();
+// }
+
+
+//const edad = "18";
+//operadores de igualdad
+
+// if (edad != 18) {
+//     console.log("Tiene 18 años!!");
+// }else {
+//     console.log("no tienes 18 años");
+// }
+
+//Conversiön de tipos de datos 
+// const number1 = 5;
+// const number2 = 20;
+
+// console.log(Number(number1) + Number(number2));
+// console.log(parseInt(number1) + parseInt(number2));
+
+// const result = number1.toString();  // strintify parseString
+// console.log(typeof result);
+
+// const edad = 19;
+// if (edad > 18); {
+//      //> >=  <  <=
+// console.log ("edad es igual o mayor a 18 años");
+// } else {
+//     console.log ("No eres mayor a 18 años");
+// }
+
+
+// const text = "hola" ;
+
+// if (text === "hola") {
+//     console.log("hola que tal, en que puedo ayudarte");
+// } else if (text == "Transferirme a un asesor"){
+//     console.log ("claro!, te transferirë enseguida");
+// } else if (text == "Chao"){
+//     console.log ("Fue un placer,Adios");
+// }else {
+//     console.log("lo siento, no puedo comprender");
+// }
+
+
+//Operadores Lógicos and (&&) y el or (||)
+// const animal = "perro" 
+
+// if (animal == "perro" || animal == "gato" || animal == "vaca") {
+//     console.log("mamifero");
+// } else {
+//     console.log("No esta dentro de nuestro listado de mamiferos")
+// }
+
+
+// const dia == "sabado";
+
+// if (dia === "sabado" && dia === "domingo" && dia === "Festivo") {
+//     console.log("dia de descanso");
+// }   else {
+//     console.log("A trabajar");
+
+// }
+
+// const edad = 18; 
+// const liciencia = true; 
+
+// if (edad >= 18 && liciencia) {  //(edad >= 18 && liciencia === true)
+//     console.log("Puedes conducir");
+// }   else {
+//     console.log("no puedes conducir");
+// }
+
+// console.log (edad >= 20);
+// Condicional terminal
+//condicion ? true : false 
+
+// (edad >= 18 && liciencia) || isAgenteTransito  ?  console.log("puede conducir") : console.log("no puede conducir");
+
+//FUNCIONES
+// Es un bloque de codigo que podemos reutilizar
+
+// funtion sumar (){}//function
+
+// () => {} // Arrow function anonimo || funcion flecha anonima 
+// const sumarF = () => {}; 
+
+
+// 1 programa que ayude a preparar arroz
+// console.log ("Preparar agua");
+// console.log ("preparando arroz")
+// console.log ("Poner la olla en la estufa");
+// console.log ("Encender la estufa");  //...10000
+
+
+// prepararArroz (18, "Manizales"); //Manizales
+// prepararArroz (20, "Bogota"); //Bogota
+// prepararArroz (30, "Cartagena"); //Cartagena
+// prepararArroz (40, "Armenia"); //Armenia
+
+
+// function prepararArroz (grados, ciudad) { //camelCase
+//     // console.log ("Preparar agua");
+//     // console.log ("preparando arroz")
+//     // console.log ("Poner la olla en la estufa");
+//     console.log ('Para la ${ciudad} encender la estufa a ${grados}°' );
+// }
+
+
